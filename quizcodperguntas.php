9bb5369d05a4eb668a31728ce0d2dbf476f93fc0
guilhermedl12 <g.dionizio@escolar.ifrn.edu.br>
2023-06-07 15:10:48 -0300

    questao 1 correção

--- quizcodperguntas.php
+++ quizcodperguntas.php
@@ -1,10 +1,10 @@
 <?php 
 
 $pergunta[0]['questoes']="1- Quantos continentes existem na Terra?";
-$pergunta[0][0]=array(" Oito",true);
+$pergunta[0][0]=array(" Oito",false);
 $pergunta[0][1]=array(" Cinco",false);
 $pergunta[0][2]=array(" Sete",false);
-$pergunta[0][3]=array(" Seis",false);
+$pergunta[0][3]=array(" Seis",true);
 
 
 
