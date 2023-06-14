cb89bb2e56b802f3a32e370f82ee7bb73834f368
guilhermedl12 <g.dionizio@escolar.ifrn.edu.br>
2023-06-14 16:11:29 -0300

    css adicionado

--- quizcodperguntas.php
+++ quizcodperguntas.php
@@ -107,6 +107,8 @@ function exibirQuestao($ordem){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="quiz.css"  />
+
     <title>QUIZ</title>
 </head>
 <body>
